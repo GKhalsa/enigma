b27ae1cd5d5d612bc330148e4da46ed5c5f25c53
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2015-12-13 14:34:39 -0700

    create cypher

---
+++ cypher.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Cypher
+  # attr_reader :day, :month, :year  # => nil
+
+  def initialize
+    time = Time.new
+    @date = time.strftime("%d%m%y")
+  end
+
+  def todays_date
+    date = @date
+    date.to_i
+  end
+
+  def squared
+    date = @date
+     date.to_i ** 2
+  end
+
+
+
+
+
+end
---
+++ cypher_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require_relative 'cypher'
+require 'pry'
+
+class CypherTest < Minitest::Test
+  def test_take_the_date
+    date = Cypher.new
+    assert_equal 131215, date.todays_date
+  end
+
+  def test_square_the_date
+    date = Cypher.new
+    assert_equal 17217376225 , date.squared
+  end
+
+  def test_get_last_four_numbers_from_squaredate
+    date = Cypher.new
+    
+
+
+
+
+
+
+end
