5e256535c3a436f2c8e98c3b3257b7b90ad53b60
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2015-12-14 12:15:15 -0700

    update encrypt

--- cypher.rb
+++ cypher.rb
@@ -1,11 +1,15 @@
-require 'pry'
+require 'pry'  # => true
+
 
 class Cypher
-  # attr_reader :day, :month, :year  # => nil
+  attr_reader :test_key  # => nil
+  #attr_accessor :input
 
-  def initialize
-    time = Time.new
-    @date = time.strftime("%d%m%y")
+  def initialize(input="")
+    time = Time.new                  # => 2015-12-13 21:41:18 -0700
+    @date = time.strftime("%d%m%y")  # => "131215"
+    @test_key = 41521                # => 41521
+    @input = input                   # => "ab"
   end
 
   def todays_date
@@ -15,11 +19,76 @@ class Cypher
 
   def squared
     date = @date
-     date.to_i ** 2
+    date.to_i ** 2
+  end
+
+  def last_four
+    square_string = squared.to_s
+    square_split = square_string.split(//)
+    slice_four = square_split.slice(-4..-1)
+    slice_four.join.to_i
+  end
+
+  def rotation_a
+    rotation = @test_key.to_s
+    rotation_a = rotation[0..1]
+    rotation_a = rotation_a.to_i
+  end
+
+  def offset_a
+    offset = last_four.to_s
+    offset_a = offset[0]
+    offset_a = offset_a.to_i
+  end
+
+  def full_rotation_a
+    rotation_a + offset_a
+  end
+
+  def full_rotation_b
+    rotation = @test_key.to_s
+    rotation_b = rotation[1..2]
+    rotation_b = rotation_b.to_i
+    offset_b = last_four.to_s
+    offset_b = offset_b[1]
+    offset_b = offset_b.to_i
+    full_rotation_b = rotation_b + offset_b
+  end
+
+  def full_rotation_c
+    rotation = @test_key.to_s
+    rotation_c = rotation[2..3]
+    rotation_c = rotation_c.to_i
+    offset_c = last_four.to_s
+    offset_c = offset_c[2]
+    offset_c = offset_c.to_i
+    full_rotation_c = rotation_c + offset_c
+  end
+
+  def full_rotation_d
+    rotation = @test_key.to_s
+    rotation_d = rotation[3..4]
+    rotation_d = rotation_d.to_i
+    offset_d = last_four.to_s
+    offset_d = offset_d[3]
+    offset_d = offset_d.to_i
+    full_rotation_d = rotation_d + offset_d
+  end
+
+  def place_value
+
+    input = @input.split(//)                                                                                                                                                    # => ["a", "b"]
+    characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"," ",".",","]  # => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","]
+    inputs = input.map do |i|                                                                                                                                                   # => ["a", "b"]
+      characters.index(i)                                                                                                                                                       # => 0, 1
+    end                                                                                                                                                                         # => [0, 1]
+
+
   end
 
 
 
-git hub test
 
 end
+
+    #  tes = Cypher.new("ab").place_value  # => [0, 1]
--- cypher_test.rb
+++ cypher_test.rb
@@ -1,26 +1,77 @@
-require 'minitest/autorun'
-require_relative 'cypher'
+require 'minitest/autorun'  # => true
+require_relative 'cypher'   # ~> ArgumentError: wrong number of arguments (0 for 1)
 require 'pry'
 
 class CypherTest < Minitest::Test
-  def test_take_the_date
-    date = Cypher.new
-    assert_equal 131215, date.todays_date
+  def setup
+    @cypher = Cypher.new           # => #<Cypher:0x007fd08506c810 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd085057e38 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd085846a90 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd085845e10 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd085844e48 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd0858376f8 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd08418f680 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd08418ec58 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd08418d4e8 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd08418cd40 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd08418e708 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd08416ff60 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd08514c2d0 @date="131215", @test_key=41521, @input="">, #<Cypher:0x007fd08513e6f8 @date="131215", @test_key=41521, @input="">
   end
 
-  def test_square_the_date
-    date = Cypher.new
-    assert_equal 17217376225 , date.squared
+  # def test_take_the_date
+  #   assert_equal 131215, @cypher.todays_date  # => true
+  # end
+  #
+  # def test_square_the_date
+  #   assert_equal 17217376225 , @cypher.squared  # => true
+  # end
+  #
+  # def test_get_last_four_numbers_from_squaredate
+  #   assert_equal 6225, @cypher.last_four          # => true
+  # end
+  #
+  # def test_sample_key
+  #   assert_equal 41521, @cypher.test_key  # => true
+  # end
+  #
+  # def test_encryption_for_rotation_a
+  #   assert_equal 41, @cypher.rotation_a  # => true
+  # end
+  #
+  # def test_encryption_for_offset_a
+  #   assert_equal 6, @cypher.offset_a  # => true
+  # end
+  #
+  # def test_encryption_for_rotation_plus_offset_a
+  #   assert_equal 47, @cypher.full_rotation_a      # => true
+  # end
+  #
+  # def test_encryption_for_rotation_plus_offset_b
+  #   assert_equal 17, @cypher.full_rotation_b      # => true
+  # end
+  #
+  # def test_encryption_for_rotation_plus_offset_c
+  #   assert_equal 54, @cypher.full_rotation_c      # => true
+  # end
+  #
+  # def test_encryption_for_rotation_plus_offset_d
+  #   assert_equal 26, @cypher.full_rotation_d      # => true
+  # end
+  #
+  # def test_find_place_value_of_letter
+  #   z = Cypher.new("z")
+  #   assert_equal 25, z.place_value
+  # end
+  #
+  # def test_find_place_value_of_symbol
+  #   period = Cypher.new(".")
+  #   assert_equal 37, period.place_value
+  # end
+  #
+  # def test_find_place_value_of_space
+  #
+  #   space = Cypher.new(" ")
+  #   assert_equal 36, space.place_value
+  # end
+  #
+  def test_find_value_of_two_characters
+    characters = Cypher.new("ab")
+    assert_equal [0,1], characters.place_value
   end
 
-  def test_get_last_four_numbers_from_squaredate
-    date = Cypher.new
-
-    git hub test
-
-
-
-
+  def test_find_value_of_two_characters_and_space
+    characters = Cypher.new("ab ")
+    assert_equal [0,1,36], characters.place_value
+  end
 
 
 end
